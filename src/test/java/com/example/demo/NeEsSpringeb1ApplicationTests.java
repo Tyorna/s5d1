3de6aef6fe650b09651edfb211040d43fbb19910
Francesca Arata <francescaarata@live.it>
2023-08-01 17:16:49 +0200

    test con strano errore

--- src/test/java/com/example/demo/NeEsSpringeb1ApplicationTests.java
+++ src/test/java/com/example/demo/NeEsSpringeb1ApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,7 +20,9 @@ class NeEsSpringeb1ApplicationTests {
 	@Test
 	void contextLoads() {
 		try {
-			mockMvc.perform(MockMvcRequestBuilders.get("/italiano")).andExpect(status().is2xxSuccessful());
+			 String response = "Puoi prenotare più postazioni ma mai due nello stesso giorno. Nel caso la postazione fosse gia prenotata non ti farà eseguire la prenotazione";
+		        mockMvc.perform(MockMvcRequestBuilders.get("/info/italiano"))
+		                .andExpect(MockMvcResultMatchers.content().string(response));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
